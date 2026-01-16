2463c26351ac83d458f68beceaba7f0f18bd32c9
Abdullah Umar Babsel <abdullah.umar@axiatadigitallabs.com>
2026-01-17 00:01:09 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello Go!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello Go!")
+	})
+
+	log.Println("Starting server on :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
