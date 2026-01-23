5609b308a3d5641294eb67ba3db91dbb0b363fa8
Abdullah Umar Babsel <abdullah.umar@axiatadigitallabs.com>
2026-01-23 18:20:46 +0700

    feat: add throttle and update docker

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
 import articleRoutes from "./routes/article.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import slowDown from "express-slow-down";
 
 const app = express();
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 menit
+  delayAfter: 100, // setelah 100 request
+  delayMs: () => 500, // tiap request berikutnya delay 500ms
+});
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+  speedLimiter,
+);
 
 app.use(express.json());
 app.use(errorHandler);
 
 app.use("/auth", authRoutes);
-// app.use("/users", userRoutes);
 app.use("/articles", articleRoutes);
 
 export default app;
